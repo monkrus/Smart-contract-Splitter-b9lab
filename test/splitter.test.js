3af6b08603715da3401f1db05418dc1de1620418
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-01 22:21:11 +0200

    updates regarding comments

--- test/splitter.test.js
+++ test/splitter.test.js
@@ -5,9 +5,7 @@ const Splitter = artifacts.require("Splitter");
 
 contract('Splitter', accounts => {
     // Setup accounts
-    const sender = accounts[1];
-    const Bob = accounts[2];
-    const Alice = accounts[3];
+    const [sender, Bob, Alice] = accounts;
 
     let splitterInstance;
 
@@ -16,7 +14,6 @@ contract('Splitter', accounts => {
     });
 
     describe('Splitting functionality', () => {
-
         it('Should split amount evenly', async () => {
             // Split sent ether between receivers.
             const amount = 10;
@@ -26,13 +23,13 @@ contract('Splitter', accounts => {
             // Checking if everything as expected
             await checkFunds(Bob, amount / 2);
             await checkFunds(Alice, amount / 2);
+        });
 
-            it('Should split odd number of ether correctly', async () => {
-                // Split sent ether between receivers.
-                const amount = 11;
-                const txObject = await splitterInstance.split(Bob, Alice, {from: sender, value: amount});
-                assert(txObject.receipt.status, 'Split failed');
-            });
+        it('Should split odd number of ether correctly', async () => {
+            // Split sent ether between receivers.
+            const amount = 11;
+            const txObject = await splitterInstance.split(Bob, Alice, {from: sender, value: amount});
+            assert(txObject.receipt.status, 'Split failed');
         });
 
     });
